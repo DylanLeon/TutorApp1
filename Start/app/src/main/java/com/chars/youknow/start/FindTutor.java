48da7f29262a154bcb2c0ec96513a47b0a1860d0
charltonraven <williamscharlton@hotmail.com>
2016-10-04 23:22:59 -0400

    First Upload

---
+++ Start/app/src/main/java/com/chars/youknow/start/FindTutor.java
@@ -0,0 +1,17 @@
+package com.chars.youknow.start;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.GridView;
+
+public class FindTutor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_tutor);
+
+      //  GridView gv=(GridView) findViewById(R.id.TutorGrid);
+
+    }
+}
